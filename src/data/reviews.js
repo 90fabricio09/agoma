62b7b69ed51c741c563f5f472df0047fe33c7691
90fabricio09 <90934129+90fabricio09@users.noreply.github.com>
2025-11-27 11:17:14 -0300

    Update reviews.js

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -2,20 +2,20 @@
 // Você pode copiar reviews diretamente do Google Maps e colar aqui
 
 export const reviews = [
-  
- {
-    name: "Fabricio Bettarello",
-    rating: 5,
-    text: "Excelente experiência no AGOMA.! Fui super bem atendido e o lanche Cheddar D'AGOMA é simplesmente o melhor da região. Recomendo muito!",
-    date: "2025-09-01",
-    avatar: "https://media.discordapp.net/attachments/1257361928532135936/1399524205179174932/Betta_Icon.png?ex=690e777c&is=690d25fc&hm=c1b61c29648b720ac9d617edf912536a5c813a4d2472e7df696fc1b903286731&=&format=webp&quality=lossless&width=990&height=990"
- },  
+
  {
     name: "HELLEN ESTER CRESTANI LIMA",
     rating: 5,
     text: "Tudo mais do que incrível, me senti em casa e comida de qualidade, produtos frescos",
     date: "2025-10-01",
     avatar: ""
+ },  
+ {
+    name: "Fabricio Bettarello",
+    rating: 5,
+    text: "Excelente experiência no AGOMA.! Fui super bem atendido e o lanche Cheddar D'AGOMA é simplesmente o melhor da região. Recomendo muito!",
+    date: "2025-09-01",
+    avatar: "https://media.discordapp.net/attachments/1257361928532135936/1399524205179174932/Betta_Icon.png?ex=690e777c&is=690d25fc&hm=c1b61c29648b720ac9d617edf912536a5c813a4d2472e7df696fc1b903286731&=&format=webp&quality=lossless&width=990&height=990"
  },  
  {
     name: "joão victor alcântara",
@@ -31,6 +31,7 @@ export const averageRating = 0; // Exemplo: 4.8
 
 // Total de avaliações (opcional)
 export const totalRatings = 0; // Exemplo: 127
+
 
 
 
